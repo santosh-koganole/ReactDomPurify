a8034749c1dc6638793cf0834739729ccebac969
Santosh.Koganole <santosh.koganole@trelleborg.com>
2023-06-27 16:36:38 +0530

    removed unused vars

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,9 @@ function App() {
   const html = `<a href="javascript:alert('Ha ha Attack HTML !!!')">Hello!</a>`;
   //const html = `<a href="#" onClick="alert('Hello')">Hello!</a>`;
 
-  const htmlSanatized = DOMPurify.sanitize(html);
-
   const img = ` <img  src='1' alt='nothing to show' onerror="alert('Ha ha Attack HTML IMG !!!')"/>`;
-  const imgSanatized = DOMPurify.sanitize(img);
-
-  const html2 = "This seal series is locked by <b>Neha Pradhan</b>";
-  const html2Sanitized = DOMPurify.sanitize(html2);
 
   const html3 = `<script>alert('Ha ha Attack HTML3 ')</script>`;
-  const html3Sanitized = DOMPurify.sanitize(html3);
 
   return (
     <div>
@@ -27,12 +20,12 @@ function App() {
       <div dangerouslySetInnerHTML={{ __html: html3 }}></div>
 
       {/* After sanitization */}
-      {/* <div dangerouslySetInnerHTML={{ __html: htmlSanatized }}></div>
-      <div dangerouslySetInnerHTML={{ __html: imgSanatized }}></div>
-      <div dangerouslySetInnerHTML={{ __html: html3Sanitized }}></div> */}
+      {/* <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html) }}></div>
+      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(img) }}></div>
+      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(html3) }}></div> */}
 
       <textarea style={{ width: "100%", height: "400px" }}>
-        {htmlSanatized}
+        {DOMPurify.sanitize(html)}
       </textarea>
     </div>
   );
